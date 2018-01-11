199265159517786aef869da02f8792fb38840b57
Alexei Colin <ac@alexeicolin.com>
2018-01-11 17:00:22 -0500

    main: report button click events

--- show_main/show_main.ino
+++ show_main/show_main.ino
@@ -205,6 +205,8 @@ void readBtn()
         }
 
         if (digitalRead(A1) && (btn2Releases == 0)) {
+                if (btn2Pushed)
+                    Serial.println("BTN:2");
                 btn2Releases = 1;
                 btn2Presses = 0;
                 btn2Pushed = 0;
@@ -224,6 +226,8 @@ void readBtn()
         }
 
         if (digitalRead(7) && (btn0Releases == 0)) {
+                if (btn0Pushed)
+                    Serial.println("BTN:0");
                 btn0Releases = 1;
                 btn0Presses = 0;
                 btn0Pushed = 0;
@@ -243,6 +247,8 @@ void readBtn()
         }
 
         if (digitalRead(A0) && (btn1Releases == 0)) {
+                if (btn1Pushed)
+                    Serial.println("BTN:1");
                 btn1Releases = 1;
                 btn1Presses = 0;
                 btn1Pushed = 0;
